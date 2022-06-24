5b0a1bb413b5de3edae69042fd686f1842bfe2ae
sixu <969669232@qq.com>
2022-06-24 16:01:17 +0800

    刚刚导入前段文件，还没编译

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 		router.NoRoute(func(c *gin.Context) { //设置404页面
 			path := c.Request.URL.Path        //获取请求路径
 			if strings.HasPrefix(path, "/") { //如果请求路径是静态文件目录
-				reader, _ := staticFiles.Open("index.html")                            //打开静态文件目录下的index.html
+				reader, _ := staticFiles.Open("index1.html")                           //打开静态文件目录下的index.html
 				defer reader.Close()                                                   //关闭文件
 				stat, _ := reader.Stat()                                               //获取文件信息
 				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil) //设置响应内容
@@ -40,9 +40,9 @@ func main() {
 	chSignal := make(chan os.Signal, 1)                      //创建一个信号接收通道
 	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM) //注册信号接收函数
 
-	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"        //设置Chrome路径
-	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/static/index1.html") //创建Chrome命令
-	cmd.Start()                                                                       //启动Chrome
+	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"       //设置Chrome路径
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/static/index.html") //创建Chrome命令
+	cmd.Start()                                                                      //启动Chrome
 
 	select { //等待信号
 	case <-chSignal: //如果收到信号
